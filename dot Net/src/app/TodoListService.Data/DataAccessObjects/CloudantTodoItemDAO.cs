c82238ccd1ff868320297356cd7a502752038e30
Andrew Walker <alwalker@gmail.com>
2012-07-17 20:50:27 -0500

    Started data abstractions and cloudant data layer

---
+++ dot Net/src/app/TodoListService.Data/DataAccessObjects/CloudantTodoItemDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoListService.Data.DataEntities;
+using TodoListService.Data.Interfaces;
+using DreamSeat;
+
+namespace TodoListService.Data.DataAccessObjects
+{
+    public class CloudantTodoItemDAO : ITodoItemDAO
+    {
+        public IList<TodoItemDE> GetAllTodos()
+        {
+            var client = new CouchClient(
+        }
+    }
+}
---
+++ dot Net/src/app/TodoListService.Data/DataEntities/TodoItemDE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListService.Data.DataEntities
+{
+    public class TodoItemDE
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool Complete { get; set; }
+        public IList<TodoCategoryDE> Categories { get; set; }
+    }
+}
